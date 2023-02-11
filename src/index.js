b291110b374235966d6721eb579676760b45cec9
seregaxxl <seregaxxl@gmail.com>
2023-02-11 10:35:07 +0300

    render props, props children

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, {StrictMode} from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Button} from './App';
+import styled from 'styled-components';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import './bootstrap.min.css';
+
+const BigButton = styled(Button)`
+    margin: 0 auto;
+    width: 245px;
+    text-align: center;
+`;
+
+ReactDOM.render(
+  <StrictMode>
+      <App/>
+      <BigButton as="a">Отправить отчет</BigButton>
+  </StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
