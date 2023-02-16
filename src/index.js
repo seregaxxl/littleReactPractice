8b5a788ea09082f1d7d71a70b8197eced6bb6c65
seregaxxl <seregaxxl@gmail.com>
2023-02-16 10:34:26 +0300

    portals

--- src/index.js
+++ src/index.js
@@ -2,21 +2,14 @@ import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Button} from './App';
 import styled from 'styled-components';
 
 import './bootstrap.min.css';
 
-const BigButton = styled(Button)`
-    margin: 0 auto;
-    width: 245px;
-    text-align: center;
-`;
 
 ReactDOM.render(
   <StrictMode>
       <App/>
-      <BigButton as="a">Отправить отчет</BigButton>
   </StrictMode>,
   document.getElementById('root')
 );
