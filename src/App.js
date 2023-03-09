5d5afbb75a2d236d74acd44baf3631037cbd823a
seregaxxl <seregaxxl@gmail.com>
2023-03-09 13:31:32 +0300

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,71 +1,143 @@
-import React, {Component} from 'react';
-import { createPortal } from 'react-dom';
+// import React, {Component} from 'react';
+// import { createPortal } from 'react-dom';
+// import {Container} from 'react-bootstrap';
+// import './App.css';
+
+// class Form extends Component {
+//     state = {
+//         advOpen: false
+//     }
+//     componentDidMount() {
+//         setTimeout(this.handleClick, 3000)
+//     }
+
+//     handleClick = () => {
+//         this.setState(({advOpen}) => ({
+//             advOpen: !advOpen
+//         }))
+//         console.log('click');
+//     }
+
+//     render() {
+//         return (
+//             <Container>
+//                 <form onClick={this.handleClick} className="w-50 border mt-5 p-3 m-auto" 
+//                 style={{'overflow': 'hidden', 
+//                         'position': 'relative'}}>
+//                     <div className="mb-3">
+//                         <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+//                         <input  type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+//                     </div>
+//                     <div className="mb-3">
+//                         <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+//                         <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+//                     </div>
+//                     {
+//                         this.state.advOpen ? 
+//                         <Portal>
+//                             <Msg/>
+//                         </Portal> : null
+//                     }
+                    
+//                 </form>
+//             </Container>
+//         )
+//     }
+// }
+// const Portal = (props) => {
+//     const node = document.createElement('div');
+//     document.body.appendChild(node);
+//     return createPortal(props.children, node);
+// }
+// const Msg = () => {
+//     return (
+//         <div 
+//                     style={{'width': '500px', 
+//                             'height': '150px', 
+//                             'backgroundColor': 'red', 
+//                             'position': 'absolute', 
+//                             'right': '0', 
+//                             'bottom': '0'}}>
+//                         Hello
+//                     </div>
+//     )
+// }
+
+// function App() {
+//     return (
+//         <Form/>
+//     );
+// }
+
+// export default App;
+
+
+import {useState, useReducer} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
-class Form extends Component {
-    state = {
-        advOpen: false
-    }
-    componentDidMount() {
-        setTimeout(this.handleClick, 3000)
-    }
-
-    handleClick = () => {
-        this.setState(({advOpen}) => ({
-            advOpen: !advOpen
-        }))
-        console.log('click');
-    }
-
-    render() {
-        return (
-            <Container>
-                <form onClick={this.handleClick} className="w-50 border mt-5 p-3 m-auto" 
-                style={{'overflow': 'hidden', 
-                        'position': 'relative'}}>
-                    <div className="mb-3">
-                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                        <input  type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                    {
-                        this.state.advOpen ? 
-                        <Portal>
-                            <Msg/>
-                        </Portal> : null
-                    }
-                    
-                </form>
-            </Container>
-        )
+function reducer (state, action) {
+    switch (action.type) {
+        case 'toggle':
+            return {autoplay:!state.autoplay}
+        case 'slow' :
+            return {autoplay:300}
+        case 'fast' :
+            return {autoplay:700}
+        case 'custom' :
+            return {autoplay:action.payload}
+        default :
+            throw new Error();
     }
 }
-const Portal = (props) => {
-    const node = document.createElement('div');
-    document.body.appendChild(node);
-    return createPortal(props.children, node);
+
+function init (initial) {
+    return {autoplay:initial}
 }
-const Msg = () => {
+
+const Slider = ({initial}) => {
+    const [slide, setSlide] = useState(0);
+    // const [autoplay, setAutoplay] = useState(false);
+    const [autoplay, dispatchAutoplay] = useReducer(reducer, initial, init);
+
+    function changeSlide(i) {
+        setSlide(slide => slide + i);
+    }
+
     return (
-        <div 
-                    style={{'width': '500px', 
-                            'height': '150px', 
-                            'backgroundColor': 'red', 
-                            'position': 'absolute', 
-                            'right': '0', 
-                            'bottom': '0'}}>
-                        Hello
-                    </div>
+        <Container>
+            <div className="slider w-50 m-auto">
+                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <div className="text-center mt-5">Active slide {slide} <br/>{autoplay.autoplay ? autoplay.autoplay : null} </div>
+                <div className="buttons mt-3">
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(-1)}>-1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(1)}>+1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatchAutoplay({type:'toggle'})}>toggle autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatchAutoplay({type:'slow'})}>slow autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatchAutoplay({type:'fast'})}>fast autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={(e) => dispatchAutoplay({type:'custom', payload: +e.target.textContent})}>1000</button>
+
+                </div>
+            </div>
+        </Container>
     )
 }
 
 function App() {
     return (
-        <Form/>
+        <Slider initial={false}/>
     );
 }
 
